73a25b4d7f7111d3c48a11be687b91c88069fdad
Sh1ynx <sh1ynx@proton.me>
2026-08-12 15:38:38 +0300

    health endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hi")
+import { createServer } from 'http';
+
+
+const server = createServer((req: any, res: any) => {
+    if (req.url == "/health" && req.method == "GET"){
+        res.writeHead(200, {"Content-Type": "application/json"})
+        res.write(JSON.stringify({"message":"I am alive"}))
+        res.end()
+    }
+    else{
+        res.writeHead(404, {"Content-Type": "application/json"})
+        res.write(JSON.stringify({"message": "Not found"}))
+        res.end()
+    }
+})
+
+server.listen(3000, () => {
+    console.log("welcome")
+})
+
